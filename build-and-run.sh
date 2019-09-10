dcd4742ded6ce697e2441a337a44fec17d464bfe
rexim <reximkut@gmail.com>
2019-09-11 00:58:34 +0700

    (#16) Reject any device that is not 046d:c218

--- build-and-run.sh
+++ build-and-run.sh
@@ -12,4 +12,9 @@ popd
 make -B
 
 # Run QEMU
+
+# Gamepad
 sudo qemu-system-i386 -usb -device usb-host,vendorid=0x046d,productid=0xc218 -cdrom test.iso
+
+# Wacom
+# sudo qemu-system-i386 -usb -device usb-host,vendorid=0x056a,productid=0x030e -cdrom test.iso
